a80032b6faffdffb29854a97155bd123bc96ca37
kf4btg <Kf4btg@users.noreply.github.com>
2014-12-23 22:46:29 -0500

    miraculously managed to implement a dynamic sorting functionality

--- IHOrganizer.cs
+++ IHOrganizer.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using TAPI;
 using Terraria;
 
@@ -11,17 +12,17 @@ namespace InvisibleHand
     public static class IHOrganizer
     {
 
-        private static readonly Dictionary<ItemSortProperty,String> sortParams;
-
-        public static void Init(){
-            sortParams=new Dictionary<ItemSortProperty,String>();
-
-            // sortParams.Add(ItemSortProperty.Value, "value");
-            foreach (ItemSortProperty isp in ItemSortProperty)
-            {
-
-            }
-        }
+        // private static readonly Dictionary<ItemSortProperty,String> sortParams;
+        //
+        // public static void Init(){
+        //     sortParams=new Dictionary<ItemSortProperty,String>();
+        //
+        //     // sortParams.Add(ItemSortProperty.Value, "value");
+        //     foreach (ItemSortProperty isp in ItemSortProperty)
+        //     {
+        //
+        //     }
+        // }
 
 
         // this will sort the categorized items first by category, then by
@@ -35,12 +36,14 @@ namespace InvisibleHand
                 orderby category.Key
                 select category;
 
+            // and that was the easy part. Now need to dynamically construct Queries using
+            // LINQ expression methods with arbitrary (maybe later user-defined) sorting parameters.``
             foreach (var category in byCategory)
-            {
+            {   // category.Key is IMCategory<Item>
+
                 switch(category.Key.catID)
                 {
                     case ItemCat.ORE:
-                        oreSort(category);
                         break;
                 }
             }
@@ -54,27 +57,84 @@ namespace InvisibleHand
             List<String> props = new List<String>();
             foreach (String s in (p.ToString().Split(new Char[] {',',' '}, StringSplitOptions.None)))
             {
+                // first letter to lowercase, add to list
                 props.Add(String.Concat((s[0].ToString().ToLower()),s.Substring(1)));
             }
             return props;
         }
 
-        public static void BuildQuery(IEnumerable<IComparable<Object>> preds)
-        {
+        // public static void BuildQuery(IEnumerable<IComparable<Object>> preds)
+        // public static void BuildQuery(IGrouping<IMCategory<Item>,CategorizedItem> source, List<String> sortFields)
+        public static IQueryable<Item> BuildQuery(IQueryable<Item> source, List<String> sortFields)
+                {
+            //category type is IGrouping<IMCategory<Item>,CategorizedItem>
+            /* Build an expression of the form
+                from item in category
+                    orderby item.property1,
+                            item.property2,
+                            ...
+                    select item
+                OR
+                items.OrderBy(item => item.property1).ThenBy(item => item.property2)....
+            */
+
+            //Create the Parameter Expression for the query
+            // Expressions.ParameterExpression
+            var eachItemAsParam = Expression.Parameter(typeof(Item), "item");
+
+            //get the first orderBy expression
+            MemberExpression orderByProperty;
+
+            // Prepare Expression var
+            MethodCallExpression orderByExp;
+
+            bool first = true;
+            foreach (String s in sortFields)
+            {
+                orderByProperty = Expression.Property(eachItemAsParam, s);
+
+                if (first)
+                {   //create expression to represent source.OrderBy(...)
+                    first=false;
+                    orderByExp = Expression.Call(
+                        typeof(Queryable),
+                        "OrderBy",
+                        new Type[] { source.ElementType, typeof(IComparable) },
+                        source.Expression,
+                        Expression.Lambda<Func<Item, IComparable>>(orderByProperty, new ParameterExpression[] { eachItemAsParam })
+                        );
+                }
+                else
+                {
+                    // Create similar expression trees for any other specified properties, using a ThenBy call
+                    // (could make this recursive)
+
+                    orderByExp = Expression.Call(
+                        typeof(Queryable),
+                        "ThenBy",
+                        new Type[] { source.ElementType, typeof(IComparable) },
+                        orderByExp,
+                        Expression.Lambda<Func<Item, IComparable>>(orderByProperty, new ParameterExpression[] { eachItemAsParam })
+                        );
+                }
+            }
+
+            // create and return the query
+            return source.Provider.CreateQuery<Item>(orderByExp);
 
         }
 
-        public static List<CategorizedItem> oreSort(IGrouping<IMCategory<Item>,CategorizedItem> ores)
-        {
-            // List<CategorizedItem> sorted = new List<CategorizedItem>();
-
-
-        }
-
-        public static IEnumerable<CategorizedItem> sortBy(IEnumerable<CategorizedItem> items, IEnumerable<ItemSortProperty> props)
-        {
-
-        }
+        // public static List<CategorizedItem> oreSort(IGrouping<IMCategory<Item>,CategorizedItem> ores)
+        // {
+        //     // List<CategorizedItem> sorted = new List<CategorizedItem>();
+        //
+        //
+        // }
+        //
+        // public static IEnumerable<CategorizedItem> sortBy(IEnumerable<CategorizedItem> items, IEnumerable<ItemSortProperty> props)
+        // {
+        //
+        // }
 
 
     }

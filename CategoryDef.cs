dc432f9310fa89d0e4aa4deb5d7d161b84055b98
kf4btg <Kf4btg@users.noreply.github.com>
2014-12-31 13:43:35 -0500

    Adjusted category sort/check order.

--- CategoryDef.cs
+++ CategoryDef.cs
@@ -54,7 +54,7 @@ namespace InvisibleHand
         // the category matching rules. This is important due to a kind
         // of "sieve" or "cascade" effect, where items that would have matched
         // a certain category were instead caught by an earlier one.
-        public static readonly ItemCat[] CheckOrder = new ItemCat[]  {
+        public static readonly ItemCat[] CheckOrder =  {
             ItemCat.COIN,
             ItemCat.MECH,
             ItemCat.TOOL,
@@ -65,6 +65,8 @@ namespace InvisibleHand
             ItemCat.BODY,
             ItemCat.LEGS,
             ItemCat.ACCESSORY,
+            ItemCat.SPECIAL,
+            ItemCat.PET,
             ItemCat.VANITY,
             ItemCat.MELEE,
             ItemCat.BOMB,
@@ -73,12 +75,11 @@ namespace InvisibleHand
             ItemCat.MAGIC,
             ItemCat.SUMMON,
             // ItemCat.HOOK,
-            ItemCat.CONSUME,
             ItemCat.POTION,
+            ItemCat.CONSUME,
             ItemCat.BAIT,
             ItemCat.DYE,
             ItemCat.PAINT,
-            ItemCat.TILE,
             ItemCat.ORE,
             ItemCat.BAR,
             ItemCat.GEM,
@@ -91,11 +92,10 @@ namespace InvisibleHand
             ItemCat.BANNER,
             ItemCat.CLUTTER,
             ItemCat.WOOD,
-            ItemCat.BLOCK,
             ItemCat.BRICK,
+            ItemCat.BLOCK,
+            ItemCat.TILE,
             ItemCat.WALL,
-            ItemCat.PET,
-            ItemCat.SPECIAL,
             ItemCat.MISC_MAT,
             ItemCat.OTHER
         };
@@ -168,11 +168,6 @@ namespace InvisibleHand
                 TileID.ImmatureHerbs, TileID.Saplings /*Acorn*/, TileID.Pumpkins /*Pumpkin Seed*/ } );
                 // get the rest by EndsWith("Seeds")
 
-        public static readonly
-                HashSet<int> TileGroupTool       = new HashSet<int>( new int[] {
-                    TileID.Rope, TileID.Chain });
-
-
         public static void Initialize()
         {
             SetupCategories();
@@ -192,39 +187,36 @@ namespace InvisibleHand
             Categories.Add( ItemCat.PICK, 		item   	=> item.pick > 0);
             Categories.Add( ItemCat.AXE, 		item   	=> item.axe > 0);
             Categories.Add( ItemCat.HAMMER,		item   	=> item.hammer > 0);
-            Categories.Add( ItemCat.HEAD,		item 	=> item.headSlot != -1 && item.defense>0); //13=empty bucket
-            Categories.Add( ItemCat.BODY,		item 	=> item.bodySlot != -1 && item.defense>0);
-            Categories.Add( ItemCat.LEGS,		item 	=> item.legSlot  != -1 && item.defense>0);
-            Categories.Add( ItemCat.ACCESSORY,	item   	=> item.accessory && !item.vanity);
-            Categories.Add( ItemCat.VANITY,		item 	=> (item.vanity || item.headSlot!=-1 || item.bodySlot!=-1 || item.legSlot!=-1) && item.defense==0 ); //catch the non-armor
+            // Categories.Add( ItemCat.HOOK,       item    => ProjDef.byType.ContainsKey(item.shoot) && (ProjDef.byType[item.shoot].hook || ProjDef.byType[item.shoot].aiStyle==7) );
+            Categories.Add( ItemCat.TOOL,       item    => item.IsTool() );
+            Categories.Add( ItemCat.MECH,       item    => item.mech || item.cartTrack );
             Categories.Add( ItemCat.MELEE,		item 	=> item.damage > 0 && item.melee);
             Categories.Add( ItemCat.RANGED,		item 	=> item.damage > 0 && item.ranged && (item.ammo == 0));
             Categories.Add( ItemCat.BOMB,       item    => item.IsBomb());
             Categories.Add( ItemCat.AMMO, 		item   	=> item.damage > 0 && item.ranged && item.ammo != 0 && !item.notAmmo);
             Categories.Add( ItemCat.MAGIC, 		item   	=> item.damage > 0 && item.magic);
             Categories.Add( ItemCat.SUMMON,		item   	=> item.damage > 0 && item.summon);
-            Categories.Add( ItemCat.MECH,       item    => item.mech || item.cartTrack );
-
-            // Categories.Add( ItemCat.HOOK,       item    => ProjDef.byType.ContainsKey(item.shoot) && (ProjDef.byType[item.shoot].hook || ProjDef.byType[item.shoot].aiStyle==7) );
-
-            Categories.Add( ItemCat.TOOL,       item    => item.IsTool() );
-
-            Categories.Add( ItemCat.CONSUME, 	item   	=> item.consumable && item.bait == 0 && item.damage <= 0 && item.createTile == -1
-                                                        && item.tileWand == -1 && item.createWall == -1 && item.ammo == 0 && item.name != "Xmas decorations");
-
+            Categories.Add( ItemCat.PET, 		item   	=> item.damage <= 0 && ((item.shoot > 0 && Main.projPet[item.shoot])
+                                                        || (item.buffType > 0 && (Main.vanityPet[item.buffType] || Main.lightPet[item.buffType]))));
+            Categories.Add( ItemCat.HEAD,		item 	=> item.headSlot != -1 && item.defense>0); //13=empty bucket
+            Categories.Add( ItemCat.BODY,		item 	=> item.bodySlot != -1 && item.defense>0);
+            Categories.Add( ItemCat.LEGS,		item 	=> item.legSlot  != -1 && item.defense>0);
+            Categories.Add( ItemCat.ACCESSORY,	item   	=> item.accessory && !item.vanity);
+            Categories.Add( ItemCat.VANITY,		item 	=> (item.vanity || item.headSlot!=-1 || item.bodySlot!=-1 || item.legSlot!=-1) && item.defense==0 ); //catch the non-armor
             // for some reason, all vanilla potions have w=14, h=24. Food, ale, etc. are all different.
             Categories.Add( ItemCat.POTION, 	item   	=> Categories[ItemCat.CONSUME].Invoke(item) && item.width==14 && item.height==24 );
+            Categories.Add( ItemCat.CONSUME, 	item   	=> item.consumable && item.bait == 0 && item.damage <= 0 && item.createTile == -1
+                                                        && item.tileWand == -1 && item.createWall == -1 && item.ammo == 0 && item.name != "Xmas decorations");
 
             Categories.Add( ItemCat.BAIT, 		item   	=> item.bait > 0 && item.consumable);
             Categories.Add( ItemCat.DYE, 		item   	=> item.dye != 0);
             Categories.Add( ItemCat.PAINT, 		item   	=> item.paint != 0);
-            Categories.Add( ItemCat.TILE, 		item   	=> (item.createTile != -1 && item.createTile!=TileID.DyePlants) || item.tileWand != -1 || item.name == "Xmas decorations" );
             Categories.Add( ItemCat.ORE, 		item   	=> item.createTile != -1 && (item.name.EndsWith("Ore") || TileGroupOre.Contains(item.createTile)) );
             Categories.Add( ItemCat.BAR, 		item   	=> item.createTile==TileID.MetalBars );
             Categories.Add( ItemCat.GEM, 		item   	=> item.createTile==TileID.ExposedGems );
             Categories.Add( ItemCat.SEED, 		item   	=> TileGroupSeed.Contains(item.createTile) || (item.createTile != -1 && item.name.EndsWith("Seeds") ));
-            Categories.Add( ItemCat.CRAFT, 		item   	=> TileGroupCrafting.Contains(item.createTile) );
             Categories.Add( ItemCat.LIGHT, 		item   	=> TileGroupLighting.Contains(item.createTile) );
+            Categories.Add( ItemCat.CRAFT, 		item   	=> TileGroupCrafting.Contains(item.createTile) );
             Categories.Add( ItemCat.FURNITURE, 	item   	=> TileGroupFurniture.Contains(item.createTile) );
             Categories.Add( ItemCat.STATUE, 	item   	=> TileGroupStatue.Contains(item.createTile) );
             Categories.Add( ItemCat.WALLDECO, 	item   	=> TileGroupWallDeco.Contains(item.createTile) );
@@ -236,9 +228,8 @@ namespace InvisibleHand
                                                             || item.name.EndsWith("Slab") || item.name.EndsWith("Plating")) );
 
 
+            Categories.Add( ItemCat.TILE, 		item   	=> (item.createTile != -1 && item.createTile!=TileID.DyePlants) || item.tileWand != -1 || item.name == "Xmas decorations" );
             Categories.Add( ItemCat.WALL, 		item   	=> item.createWall != -1);
-            Categories.Add( ItemCat.PET, 		item   	=> item.damage <= 0 && ((item.shoot > 0 && Main.projPet[item.shoot])
-                                                        || (item.buffType > 0 && (Main.vanityPet[item.buffType] || Main.lightPet[item.buffType]))));
             Categories.Add( ItemCat.MISC_MAT,	item   	=> item.material && !item.notMaterial);
             Categories.Add( ItemCat.SPECIAL,	item   	=> item.useStyle == 4 ); //Boss summon, hearts, mana crystals
             Categories.Add( ItemCat.OTHER, 		item    => true);
@@ -249,9 +240,6 @@ namespace InvisibleHand
     			// acorn, pumpkin seed = SEED
     			//
 
-    		// TODO: divide the "OTHER" category into OTHER_MATERIALS & OTHER (stuff like umbrella, )
-    		// TODO: make bombs/dynamite & boss summoning items be separate from food and other
-    		//		  buffing consumables.
     		// TODO: see if possible to read some NPCdef file to figure out if an item is an NPC or an environment
     		//		(entity or tile) drop.
 
@@ -271,30 +259,32 @@ namespace InvisibleHand
         */
         public static void SetupSortingRules()
         {
+            ItemSortRules.Add( ItemCat.COIN,     new List<String> { "type", "stack desc"});
             ItemSortRules.Add( ItemCat.PICK,     new List<String> { "rare", "pick", "type", "value"});
             ItemSortRules.Add( ItemCat.AXE,      new List<String> { "rare", "axe", "type", "value"});
             ItemSortRules.Add( ItemCat.HAMMER,   new List<String> { "rare", "hammer", "type", "value"});
             // stack to sort the stackable boomerangs separately
             ItemSortRules.Add( ItemCat.MELEE,    new List<String> { "maxStack", "damage", "type", "rare", "value", "stack desc"});
+            // ItemSortRules.Add( ItemCat.HOOK,     new List<String> { "shoot" , "type"});
+            ItemSortRules.Add( ItemCat.TOOL,     new List<String> { "consumable", "fishingPole", "shoot", "type", "stack desc" });
             // consumable to sort throwing weapons separately
+            ItemSortRules.Add( ItemCat.MECH,     new List<String> { "cartTrack", "tileBoost", "createTile desc", "value", "type", "stack desc"});
             ItemSortRules.Add( ItemCat.RANGED,   new List<String> { "consumable", "damage", "type", "rare", "value", "stack desc"});
+            ItemSortRules.Add( ItemCat.BOMB,     new List<String> { "damage>0 desc", "type", "stack desc"});
+            ItemSortRules.Add( ItemCat.AMMO,     new List<String> { "rare", "damage", "type", "value", "stack desc"});
             ItemSortRules.Add( ItemCat.MAGIC,    new List<String> { "damage", "rare", "type", "value"});
             ItemSortRules.Add( ItemCat.SUMMON,   new List<String> { "damage", "rare", "type", "value"});
-            ItemSortRules.Add( ItemCat.AMMO,     new List<String> { "rare", "damage", "type", "value", "stack desc"});
+            ItemSortRules.Add( ItemCat.PET,      new List<String> { "buffType", "type"});
             ItemSortRules.Add( ItemCat.HEAD,     new List<String> { "rare", "defense", "value", "type"});
             ItemSortRules.Add( ItemCat.BODY,     new List<String> { "rare", "defense", "value", "type"});
             ItemSortRules.Add( ItemCat.LEGS,     new List<String> { "rare", "defense", "value", "type"});
             ItemSortRules.Add( ItemCat.ACCESSORY,new List<String> { "handOffSlot", "handOnSlot", "backSlot", "frontSlot", "shoeSlot", "waistSlot",
                                                                     "wingSlot", "balloonSlot", "faceSlot", "neckSlot", "shieldSlot",
                                                                     "rare", "value", "type", "prefix.id"});
-            // ItemSortRules.Add( ItemCat.HOOK,     new List<String> { "shoot" , "type"});
-            ItemSortRules.Add( ItemCat.TOOL,     new List<String> { "consumable", "fishingPole", "shoot", "type", "stack desc" }
             // stack because of those fishbowls...
             ItemSortRules.Add( ItemCat.VANITY,   new List<String> { "headSlot!=-1 desc", "bodySlot!=-1 desc", "legSlot!=-1 desc", "name", "type", "stack desc"});
-            ItemSortRules.Add( ItemCat.PET,      new List<String> { "buffType", "type"});
-            ItemSortRules.Add( ItemCat.MECH,     new List<String> { "cartTrack", "tileBoost", "createTile desc", "value", "type", "stack desc"});
-            ItemSortRules.Add( ItemCat.CONSUME,  new List<String> { "potion desc", "buffType", "type", "stack desc"});
             ItemSortRules.Add( ItemCat.POTION,   new List<String> { "healLife desc", "healMana desc", "buffType!=0 desc", "buffType", "type", "stack desc"});
+            ItemSortRules.Add( ItemCat.CONSUME,  new List<String> { "potion desc", "buffType", "type", "stack desc"});
             ItemSortRules.Add( ItemCat.BAIT,     new List<String> { "bait", "type", "stack desc"});
             ItemSortRules.Add( ItemCat.DYE,      new List<String> { "dye", "type", "stack desc"});
             ItemSortRules.Add( ItemCat.PAINT,    new List<String> { "paint", "type", "stack desc"});
@@ -303,8 +293,8 @@ namespace InvisibleHand
             ItemSortRules.Add( ItemCat.GEM,      new List<String> { "rare", "value", "type", "stack desc"});
             ItemSortRules.Add( ItemCat.SEED,     new List<String> { "name", "type", "stack desc"});
 
-            ItemSortRules.Add( ItemCat.CRAFT,    new List<String> { "createTile", "type", "name", "stack desc"});
             ItemSortRules.Add( ItemCat.LIGHT,    new List<String> { "createTile", "type", "name", "stack desc"});
+            ItemSortRules.Add( ItemCat.CRAFT,    new List<String> { "createTile", "type", "name", "stack desc"});
             ItemSortRules.Add( ItemCat.FURNITURE,new List<String> { "createTile", "type", "name", "stack desc"});
             ItemSortRules.Add( ItemCat.STATUE,   new List<String> { "createTile", "type", "name", "stack desc"});
             ItemSortRules.Add( ItemCat.WALLDECO, new List<String> { "createTile", "type", "name", "stack desc"});
@@ -317,7 +307,7 @@ namespace InvisibleHand
             ItemSortRules.Add( ItemCat.WALL,     new List<String> { "createWall", "type", "stack desc"});
             // generic stuff
             ItemSortRules.Add( ItemCat.MISC_MAT, new List<String> { "type", "netID", "stack desc"});
-            ItemSortRules.Add( ItemCat.SPECIAL,  new List<String> { "maxStack", "type", "stack desc"}); 
+            ItemSortRules.Add( ItemCat.SPECIAL,  new List<String> { "maxStack", "type", "stack desc"});
             // quest fish: uniquestack=true, rare=-11
             ItemSortRules.Add( ItemCat.OTHER,    new List<String> { "uniqueStack", "rare", "type", "netID", "stack desc"});
         }//end setup sorting rules

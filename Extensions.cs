b3794100719b41f4ccd4af0aed3f7357962d17a8
kf4btg <Kf4btg@users.noreply.github.com>
2015-01-22 17:53:35 -0500

    ...more color nonsense

--- Extensions.cs
+++ Extensions.cs
@@ -67,6 +67,98 @@ namespace InvisibleHand
         }
     #endregion
 
-    }   
+        public static Color Invert(this Color c)
+        {
+            return new Color(255-c.R, 255-c.G, 255-c.B, 255);
+        }
+
+        public static Color Rotate(this Color c)
+        {
+            return new Color(c.B, c.R, c.G, 255);
+        }
+
+        public static Color GetMapColor(this Point p, bool applyLighting=false)
+        {
+            return GetMapColor(new TilePoint(p), applyLighting);
+        }
+
+        public static Color GetMapColor(this TilePoint pTile, bool applyLighting=false)
+        {
+            Color mColor;
+            try
+            {
+                //this is sometimes null
+                Main.map[pTile.X,pTile.Y].getColor(out mColor, pTile.Y);
+            }
+            catch
+            {
+                mColor = Color.White;
+            }
+
+            if (applyLighting)
+            {
+                Color lighting = Lighting.GetColor(pTile.X, pTile.Y);
+                float scale = (float)(lighting.R+lighting.G+lighting.B)/3/255;
+
+                // float[] scale = new float[] {
+                //     (float)lighting.R/255,
+                //     (float)lighting.G/255,
+                //     (float)lighting.B/255};
+                //
+                // mColor.R*=(byte)(float)(lighting.R/255);
+                // mColor.G*=(byte)(float)(lighting.G/255);
+                // mColor.B*=(byte)(float)(lighting.B/255);
+                mColor*=scale;
+                // return new Color((byte)(mColor.R*scale[0]), (byte)(mColor.G*scale[1]), (byte)(texColor[0].B*scale[2]), 255);
+            }
+            return mColor;
+        }
+        // public static Color Restrict(this Color oldColor, float lower, float higher=1.0f)
+        // {
+        //     return oldColor;
+        // }
+
+    }
+
+    public struct TilePoint
+    {
+        public int X;
+        public int Y;
+
+        public int scrX;
+        public int scrY;
+
+        public TilePoint(int x, int y)
+        {
+            X=x;
+            Y=y;
+
+            scrX = (x - API.main.firstTileX)*16;
+            scrY = (y - API.main.firstTileY)*16;
+        }
+
+        // public TilePoint(Vector2 coords)
+        // {
+        //     X=(int)(coords.X/16);
+        //     Y=(int)(coords.Y/16);
+        // }
+
+        public TilePoint(Point screenPoint)
+        {
+            scrX=screenPoint.X;
+            scrY=screenPoint.Y;
+            // X=(int)(Main.screenPosition.X/16 + (float)screenPoint.X/16);
+            // Y=(int)(Main.screenPosition.Y/16 + (float)screenPoint.Y/16);
+            // X=(int)(API.main.firstTileX + (float)screenPoint.X/16);
+            X=ConvertToTileCoords(screenPoint.X, true);
+            Y=ConvertToTileCoords(screenPoint.Y, false);
+            // Y=(int)(API.main.firstTileY + (float)screenPoint.Y/16);
+        }
+
+        public static int ConvertToTileCoords(int num, bool onXaxis)
+        {
+            return onXaxis ? (int)(API.main.firstTileX + (float)num/16) : (int)(API.main.firstTileY + (float)num/16);
+        }
+    }
 
 }
--- Interface/ButtonLayer.cs
+++ Interface/ButtonLayer.cs
@@ -14,6 +14,8 @@ namespace InvisibleHand
         public Rectangle ButtonFrame { get; protected set;}
         public Point FrameCenter { get; private set;}
         private Action<SpriteBatch> onDraw;
+        private Color? prevColor = null;
+        private Color currColor;
 
 
         protected ButtonLayer(string name) : base("InvisibleHand:" + name)
@@ -26,13 +28,17 @@ namespace InvisibleHand
 
         //nope
         // protected void UpdateFrame(bool useInvertedBGColor=false)
-        protected void UpdateFrame()
+        protected void UpdateFrame(bool useBGColor = true)
         {
             foreach (var kvp in Buttons)
             {
                 ButtonFrame = (ButtonFrame.IsEmpty) ? kvp.Value.ButtonBounds : Rectangle.Union(ButtonFrame, kvp.Value.ButtonBounds);
             }
-
+            if (useBGColor)
+            {
+                FrameCenter = ButtonFrame.Center;
+                onDraw=_onDrawOverrideColor;
+            }
             // if (!ButtonFrame.IsEmpty && useInvertedBGColor)
             // {
             //     onDraw=_onDrawOverrideColor;
@@ -48,6 +54,34 @@ namespace InvisibleHand
                 kvp.Value.Draw(sb);
             }
         }
+
+        private void _onDrawOverrideColor(SpriteBatch sbc)
+        {
+            // Color ovColor = Main.bgColor; //TODO: process this to brighten up the color and make it stand out
+            // if (prevColor==null) prevColor = Main.bgColor;
+            // Color bg = Main.bgColor;
+            // // currColor = (bg.R==0 || bg.G==0 || bg.B==0 || bg.R==255 || bg.G==255 || bg.B==255) ? prevColor : bg;
+            // int s = bg.R+bg.G+bg.B;
+            // currColor = s<255 || s>600 ? prevColor ?? Color.Gray : bg;
+            // prevColor = currColor;
+
+            // TilePoint p = new TilePoint(FrameCenter);
+            // Color c;
+            // Main.map[p.X,p.Y].getColor(out c, p.Y);
+
+
+            foreach (KeyValuePair<IHAction, ButtonBase> kvp in Buttons)
+            {
+                // kvp.Value.Draw(sbc, Color.Lerp(Main.bgColor, Main.bgColor.Invert(), 0.5f));
+                // kvp.Value.Draw(sbc, Color.Lerp(Main.bgColor.Rotate(), Color.White, 0.5f));
+                // kvp.Value.Draw(sbc, currColor.Rotate().Rotate());
+                // kvp.Value.Draw(sbc, currColor);
+                kvp.Value.Draw(sbc, FrameCenter.GetMapColor(true).Invert());
+
+
+            }
+        }
+
 
         // private void _onDrawOverrideColor(SpriteBatch sbc)
         // {

cc18f387c5408a598ad37abdb6e986eb6fc3e920
kf4btg <Kf4btg@users.noreply.github.com>
2015-07-06 19:07:56 -0400

    extracted ButtonMaker class to separate file

--- Interface/ButtonLayer.cs
+++ Interface/ButtonLayer.cs
@@ -55,29 +55,4 @@ namespace InvisibleHand
             DrawButtons(sb);
         }
     }
-
-    // The Button Factory (tm)
-    // Use this to get instances of the button-layer types rather than
-    // directly calling their constructor.  This ensures that the layer
-    // frame will be properly updated.
-    public static class ButtonMaker
-    {
-        public static ButtonLayer GetButtons(String type)
-        {
-            ButtonLayer btns;
-            switch(type)
-            {
-                case "Inventory":
-                    btns = new InventoryButtons(IHBase.Instance);
-                    break;
-                case "Chest":
-                    btns = new ChestButtons(IHBase.Instance);
-                    break;
-                default:
-                    throw new ArgumentException("Invalid ButtonLayer type \"" + type + "\"; valid types are \"Inventory\" and \"Chest\".");
-            }
-            btns.UpdateFrame();
-            return btns;
-        }
-    }
 }

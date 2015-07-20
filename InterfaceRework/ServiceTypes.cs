9885d9b675cb80a1f632927708e87aa7f49b6a18
kf4btg <Kf4btg@users.noreply.github.com>
2015-07-19 21:09:48 -0400

    missed one.

--- InterfaceRework/ServiceTypes.cs
+++ InterfaceRework/ServiceTypes.cs
@@ -6,28 +6,6 @@ using TAPI;
 
 namespace InvisibleHand
 {
-    public abstract class ButtonService
-    {
-        /// A short string indicating this service's function
-        public abstract string ServiceType { get; }
-
-        /// The button to which this service's
-        /// actions will attach.
-        protected readonly CoreButton Client;
-
-        public ButtonService(CoreButton client)
-        {
-            this.Client = client;
-            // Hooks = new ButtonHooks();
-        }
-
-        /// Register required hooks with Client here
-        public abstract void Subscribe();
-        /// Remove registered hooks here
-        public abstract void Unsubscribe();
-
-    }
-
     public class LockingService<T> : ButtonService where T: CoreButton, ISocketedButton<T>
     {
         public override string ServiceType { get { return "Lock"; } }
